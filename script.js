dd52de20dd649725c143c7434f42128e7930d24e
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-06 09:33:58 -0300

    corrige event bubbling dos escutadores

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const textInput = document.getElementById('text-input');
 const memeTextPreview = document.getElementById('meme-text');
 const memeImageContainer = document.getElementById('meme-image-container');
+const memeInputInsert = document.getElementById('meme-insert');
 
 let memeText;
-function getInput() {
-  memeTextPreview.innerText = textInput.value;
+function getInput(event) {
+  if (event.target.id === 'text-input') {
+    memeTextPreview.innerText = textInput.value;
+    memeTextPreview.style.position = 'absolute';
+    memeTextPreview.style.zIndex = 9;
+  }
 }
 document.addEventListener('input', getInput);
+
+function memeInserter(event) {
+  if (event.target.id === 'meme-insert') {
+    const memeFile = memeInputInsert.files;
+    const memeImg = document.createElement('img');
+    memeImageContainer.appendChild(memeImg);
+    memeImg.src = URL.createObjectURL(memeFile[0]);
+    memeImg.style.width = '200px';
+    memeImg.style.height = '200px';
+  }
+}
+document.addEventListener('input', memeInserter);
