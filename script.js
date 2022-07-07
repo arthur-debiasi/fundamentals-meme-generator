49c8cceed4c6b7549ef211a479eaadecbf7a1730
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-06 22:00:15 -0300

    Requisito 6 important

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const buttonsDiv = document.getElementById('custom-buttons');
 const textInput = document.getElementById('text-input');
 const memeTextPreview = document.getElementById('meme-text');
-
 const previewImg = document.getElementById('meme-image');
 const memeInputInsert = document.getElementById('meme-insert');
+const memeImageContainer = document.getElementById('meme-image-container');
 
 function getInput(event) {
   if (event.target.id === 'text-input') {
@@ -20,3 +21,32 @@ function memeInserter(event) {
   }
 }
 document.addEventListener('input', memeInserter);
+
+function buttonsMemeBoard(element, text) {
+  const button = document.createElement('button');
+  buttonsDiv.appendChild(button);
+  button.id = element;
+  button.innerText = text;
+}
+buttonsMemeBoard('fire', 'Borda de Fogo');
+buttonsMemeBoard('water', 'Borda de Água');
+buttonsMemeBoard('earth', 'Borda de Terra');
+
+function fireBoard(event) {
+  if (event.target.id === 'fire') {
+    memeImageContainer.className = 'fire';
+  }
+}
+function waterBoard(event) {
+  if (event.target.id === 'water') {
+    memeImageContainer.className = 'water';
+  }
+}
+function earthBoard(event) {
+  if (event.target.id === 'earth') {
+    memeImageContainer.className = 'earth';
+  }
+}
+document.addEventListener('click', fireBoard);
+document.addEventListener('click', waterBoard);
+document.addEventListener('click', earthBoard);
